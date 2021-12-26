4fc3d9b66895150f9d91c4ea03cabc922884545c
Nicolas Duthou <nicodami19@gmail.com>
2021-12-26 18:05:36 +0100

    d7 pt 2

--- d7/d7.py
+++ d7/d7.py
@@ -1,10 +1,26 @@
 import numpy as np
+import progressbar
 
 def fuel_consumption(median,read_input):
     total_fuel = 0
     for value in read_input:
         total_fuel += abs(median - value)
     return total_fuel
+
+def fuel_consumption_pt2(read_input):
+    align_value = -1
+    total_fuel = 0
+    for i in progressbar.progressbar(range(0,max(read_input))):
+        value_fuel = 0
+        for value in read_input:
+            step_fuel = 0
+            for j in range(1,abs(i - value)+1):
+                step_fuel += j
+            value_fuel += step_fuel
+        if value_fuel < total_fuel or i == 0:
+            total_fuel = value_fuel
+            align_value = i
+    return 'Fuel used of {} at alignment value {}.'.format(total_fuel, align_value)
 
 open_input = open("d7_input.txt","r")
 
@@ -20,3 +36,6 @@ median = round(np.median(read_input))
 
 #Part 1
 print(fuel_consumption(median,read_input))
+
+#Part 2
+print(fuel_consumption_pt2(read_input))
